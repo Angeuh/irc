e666594576d0cf9419d64886de630b87f76818ae
Shak-ti <shakti.coniat@gmail.com>
2026-02-05 12:45:40 +0100

    add : script for test. Launch with ./script.sh and let it completely open without touching mouse or keyboard (~10s). Purpose : open server + 4 client and make them join #test with one of them as operator

---
+++ server/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gnome-terminal --title="IRC SERVER" -- bash -c "./SERVER 8080 123; bash"
+sleep 2
+
+xterm -e "irssi -n opUser -c localhost -p 8080" &
+sleep 2
+xdotool type "/RAWLOG OPEN debugOp.log"
+xdotool key Return
+sleep 2
+xdotool type "/join #test"
+xdotool key Return
+
+xterm -e "irssi -n riri -c localhost -p 8080" &
+sleep 2
+xdotool type "/RAWLOG OPEN debugRiri.log"
+xdotool key Return
+sleep 2
+xdotool type "/join #test"
+xdotool key Return
+xterm -e "irssi -n fifi -c localhost -p 8080" &
+sleep 2
+xdotool type "/RAWLOG OPEN debugFifi.log"
+xdotool key Return
+sleep 2
+xdotool type "/join #test"
+xdotool key Return
+xterm -e "irssi -n loulou -c localhost -p 8080" &
+sleep 2
+xdotool type "/RAWLOG OPEN debugLoulou.log"
+xdotool key Return
+sleep 2
+xdotool type "/join #test"
+xdotool key Return
